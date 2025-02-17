60a91f34a2ef36bd547033691197f2b80f87374b
thly112 <thaolythly@gmail.com>
2025-02-17 22:32:55 +0700

    Initial commit

---
+++ src/main/java/com/API/api/model/Product.java
@@ -0,0 +1,13 @@
+package com.API.api.model;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+}
