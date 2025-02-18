90295e128e6f7ee0765d13a9370e7787846413e8
thly112 <thaolythly@gmail.com>
2025-02-18 22:05:19 +0700

    api

--- src/main/java/com/API/api/ApiApplication.java
+++ src/main/java/com/API/api/ApiApplication.java
@@ -1,7 +1,9 @@
 package com.API.api;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -9,5 +11,12 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
+	// Cấu hình OpenAPI 3.0
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/**") // Bạn có thể giới hạn các path muốn tài liệu hóa
+                .build();
+    }
 }
--- src/main/java/com/API/api/model/Product.java
+++ src/main/java/com/API/api/model/Product.java
@@ -1,13 +1,56 @@
 package com.API.api.model;
 
 import java.io.Serializable;
+import java.sql.Date;
+import java.util.Locale.Category;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product implements Serializable {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long pId;
 	
+	@Column(length = 500, columnDefinition = "nvarchar(500) not null")
+	private String pName;
+	@Column(nullable = false)
+	private int quantity;
+	@Column(nullable = false)
+	private double unitPrice;
+	@Column(length = 200)
+	private String image;
+	@Column(columnDefinition = "nvarchar(500) not null")
+	private String description;
+	@Column(nullable = false)
+	private double discount;
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "YYYY-MM-DD hh:mi:ss")
+	private Date createDate;
+	@Column(nullable = false)
+	private short status;
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "cId")
+	private Category category;	
 }
